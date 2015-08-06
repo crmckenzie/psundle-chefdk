f0e4e677c480231a17a88b46bcf3096fc0fa53f9
Chris McKenzie <cmckenzie@paraport.com>
2015-08-06 13:36:05 -0700

    added synopsis to Use-ChefDk

--- Functions/Use-ChefDk.ps1
+++ Functions/Use-ChefDk.ps1
@@ -1,3 +1,8 @@
-function Use-ChefDk {
+<#
+  .SYNOPSIS
+  Configures the current powershell session to use the Chef's embedded ruby environment.
+#>
+function Use-ChefDk
+{
   chef shell-init powershell | Invoke-Expression
 }
